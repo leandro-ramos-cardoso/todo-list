5a9bbae2e3abc1c4d86bce0582237a64079c7049
Leandro Ramos Cardoso <lrcardoso.jp@gmail.com>
2025-10-21 20:36:04 -0300

    first commit

---
+++ src/main/java/br/com/leandro/todolist/user/UserController.java
@@ -0,0 +1,21 @@
+package br.com.leandro.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/")
+    public UserModel createUser(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+    }
+}
---
+++ src/main/java/br/com/leandro/todolist/user/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.leandro.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
